3ed6fd85b1240aa6c76be92447e64b28fc26b0fd
Matt Holmes <holmesm8@gmail.com>
2019-12-18 10:56:43 -0700

    Up to most popular books

--- lib/library.rb
+++ lib/library.rb
@@ -2,12 +2,13 @@ require './lib/book'
 require './lib/author'
 
 class Library
-  attr_reader :name, :books, :authors
+  attr_reader :name, :books, :authors, :checked_out_books
 
   def initialize(name)
     @name = name
     @books = []
     @authors = []
+    @checked_out_books = []
   end
 
   def add_author(author)
@@ -17,11 +18,27 @@ class Library
   end
 
   def publication_time_frame_for(author)
-    years = author.books.map { |book| book.publication_year }
+    publication_years_array = author.books.map { |book| book.publication_year }
 # require "pry"; binding.pry
-    earliest = years.min
-    latest = years.max
+    earliest = publication_years_array.min
+    latest = publication_years_array.max
 # require "pry"; binding.pry
-    testy = {:start => earliest, :end => latest}
+    timeframe = {:start => earliest, :end => latest}
   end
+
+  def checkout(book)
+    if @books.include?(book)
+      @checked_out_books << @books.delete(book)
+      # require "pry"; binding.pry
+      true
+    else
+      false
+    end
+  end
+
+  def return(book)
+   @books << @checked_out_books.delete(book)
+   require "pry"; binding.pry
+ end
+
 end
--- test/library_test.rb
+++ test/library_test.rb
@@ -39,27 +39,77 @@ class AuthorTest < Minitest::Test
     assert_equal ({:start=>"1847", :end=>"1857"}), @dpl.publication_time_frame_for(@charlotte_bronte)
     assert_equal ({:start=>"1960", :end=>"1960"}), @dpl.publication_time_frame_for(@harper_lee)
   end
+
+  def test_it_can_verify_books_have_been_checked_out
+    assert_equal false, @dpl.checkout(@mockingbird)
+    assert_equal false, @dpl.checkout(@jane_eyre)
+    assert_equal [], @dpl.checked_out_books
+    @dpl.add_author(@charlotte_bronte)
+    @dpl.add_author(@harper_lee)
+    assert_equal true, @dpl.checkout(@jane_eyre)
+    assert_equal true, @dpl.checkout(@villette)
+    assert_equal [@jane_eyre, @villette], @dpl.checked_out_books
+    @dpl.checkout(@mockingbird)
+    @dpl.return(@mockingbird)
+    assert_equal true, @dpl.checkout(@mockingbird)
+  end
 end
 
 
 
-
-# Use TDD to create a Library class that responds to the following interaction pattern.
-#
-# The publication_time_frame_for method takes an Author object as an argument and returns a hash with two key/value pairs:
-
+# Use TDD to implement the following methods on the Library class:
+# The checkout method takes a Book as an argument. It should return false if a Book does not exist in the library or it is already checked out. Otherwise, it should return true indicating that the book has been checked out.
+# The checked_out_books method should return an array of books that are currently checked out.
+# The return method takes a Book as an argument. Calling this method means that a book is no longer checked out.
+# The most_popular_book method should return the book that has been checked out the most.
+# pry(main)> require './lib/library'
+# #=> true
+# pry(main)> require './lib/author'
+# #=> true
+# pry(main)> dpl = Library.new("Denver Public Library")
+# #=> #<Library:0x00007f8c021685b0...>
+# pry(main)> charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+# #=> #<Author:0x00007f8c01429a98...>
+# pry(main)> jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+#=> #<Book:0x00007f8c01433138...>
+# pry(main)> villette = charlotte_bronte.write("Villette", "1853")
+# #=> #<Book:0x00007f8c021d84c8...>
+# # pry(main)> harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+# #=> #<Author:0x00007f8c01442520...>
+# pry(main)> mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+# #=> #<Book:0x00007f8c019506c0...>
+# # This book cannot be checked out because it doesn't exist in the library
+# pry(main)> dpl.checkout(mockingbird)
+# #=> false
+# # This book cannot be checked out because it doesn't exist in the library
+# pry(main)> dpl.checkout(jane_eyre)
+# #=> false
 # pry(main)> dpl.add_author(charlotte_bronte)
-#
 # pry(main)> dpl.add_author(harper_lee)
-#
-# pry(main)> dpl.authors
-# => [#<Author:0x00007fbeea2d78b8...>, #<Author:0x00007fbeea112730...>]
-#
-# pry(main)> dpl.books
-# => [#<Book:0x00007fbeeb3beca8...>, #<Book:0x00007fbeea8efd90...>, #<Book:0x00007fbeea24fbe8...>, #<Book:0x00007fbeeb1089f0...>]
-#
-# pry(main)> dpl.publication_time_frame_for(charlotte_bronte)
-# #=> {:start=>"1847", :end=>"1857"}
-#
-# pry(main)> dpl.publication_time_frame_for(harper_lee)
-#=> {:start=>"1960", :end=>"1960"}
+# pry(main)> dpl.checkout(jane_eyre)
+# #=> true
+# pry(main)> dpl.checked_out_books
+# => [#<Book:0x00007f8c01433138...>]
+# This book cannot be checked out because it is currently checked out
+# pry(main)> dpl.checkout(jane_eyre)
+# #=> false
+# pry(main)> dpl.return(jane_eyre)
+# Returning a book means it should no longer be checked out
+# pry(main)> dpl.checked_out_books
+# #=> []
+# pry(main)> dpl.checkout(jane_eyre)
+# #=> true
+# pry(main)> dpl.checkout(villette)
+# #=> true
+# pry(main)> dpl.checked_out_books
+# #=> [#<Book:0x00007f8c01433138...>, #<Book:0x00007f8c021d84c8...>]
+# pry(main)> dpl.checkout(mockingbird)
+# #=> true
+# pry(main)> dpl.return(mockingbird)
+# pry(main)> dpl.checkout(mockingbird)
+# #=> true
+# pry(main)> dpl.return(mockingbird)
+# pry(main)> dpl.checkout(mockingbird)
+# #=> true
+# pry(main)> dpl.most_popular_book
+# #=> #<Book:0x00007f8c019506c0...>
